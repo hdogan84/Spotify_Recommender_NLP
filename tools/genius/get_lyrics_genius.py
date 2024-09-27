fdcc5b3a6c23b3879f53d3c040394211fd2a6065
hdogan84 <dgnhkn@gmail.com>
2024-09-27 14:48:34 +0200

    rearranged the script

--- tools/genius/get_lyrics_genius.py
+++ tools/genius/get_lyrics_genius.py
@@ -5,30 +5,18 @@ import random
 from urllib.parse import urljoin
 import time
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-### This script acquires lyrics information for the data frame with 500k spotify tracks
-### Each time it is called, acquires more lyrics and saves the latest versions of both
-### the main csv file and the partition that contain lyrics. 
-### In this way, the script is suitable for scheduling and it will update the existing 
-### data each time it is called. 
-### Initially, the lyrics field is initialized as " " and ~1000 lyrics have been acquired. 
-
-df_tracks = pd.read_csv("../../data/spotify_tracks_with_lyrics.csv")
+"""This script acquires lyrics information for the data frame with 500k spotify tracks
+Each time it is called, acquires more lyrics and saves the latest versions of both the main csv file 
+and the partition that contain lyrics. 
+In this way, the script is suitable for scheduling and it will update the existing data each time it is called. 
+Initially, the lyrics field is initialized as " " and ~1000 lyrics have been acquired."""
+ 
 
 
-# Find out which tracks have lyrics field as NULL. 
-# The initial assigned value is one space character
-indx = df_tracks[df_tracks.lyrics==" "].index.tolist()
-
-# Sample a specific amount of data, to perform query for. 
-num_samples = 10
-row_indx = random.sample(indx, num_samples)
-
-ACCESS_TOKEN="GBeIy6gLeNZE6B-9CxOXyMirj7MvZWDyilSh_GYtW-bXOHfeB6lj11r0oYTHO8MQ"
-
-response_wait_time = 2 #in seconds. Between two API requests
-
-def get_song_info(song_title, artist_name):
+def get_song_info(song_title, artist_name, ACCESS_TOKEN):
     # Genius API arama URL'si
     search_url = 'https://api.genius.com/search'
     
@@ -82,35 +70,58 @@ def get_lyrics(song_url, song_name, artist):
         return lyrics
 
 
-for i in row_indx:
-    song_title = df_tracks.at[i, "Song_title"]
-    artist_name = df_tracks.at[i, "Artist"]
-    
-    song_info = get_song_info(song_title, artist_name)
+def get_genius_lyrics(song_title, artist_name, access_token):
+
+    song_info = get_song_info(song_title, artist_name, access_token)
 
     if song_info == 0:
         print(f"Genius API search NULL for: {song_title} - {artist_name}")
-        continue
-
-    #print(song_info)
+        return " "
 
     song_url = song_info['url']
-    #print(song_url)
 
     lyrics = get_lyrics(song_url, song_title, artist_name)
-    time.sleep(response_wait_time)
+    
+    return lyrics
 
-    df_tracks.at[i, "lyrics"] = lyrics
 
-# Get the dataframe with lyrics info
-df_lyrics = df_tracks[df_tracks["lyrics"]!=" "]
+if __name__ == "__main__":
 
-# Save the updated version of main data to local folder
-df_tracks.to_csv("../../data/spotify_tracks_with_lyrics.csv", index=False)
+    load_dotenv()
 
-# Save the latest version of the data with acquired lyrics
-datenow = datetime.now().strftime("%Y-%m-%d")
-timenow = datetime.now().strftime("%H-%M-%S")
-df_lyrics.to_csv(f"../../data/spotify_lyrics_{datenow}_{timenow}.csv", index=False)
+    ACCESS_TOKEN = os.getenv('GENIUS_ACCESS_TOKEN')
 
-print(len(df_lyrics))
+    response_wait_time = 2 #Between two API requests
+
+    df_tracks = pd.read_csv("../../data/spotify_tracks_with_lyrics.csv")
+
+    # Find out which tracks have lyrics field as NULL. 
+    indx = df_tracks[df_tracks.lyrics==" "].index.tolist()
+
+    # Sample a specific amount of data, to perform query for. 
+    num_samples = 10
+    row_indx = random.sample(indx, num_samples)
+
+    
+    for i in row_indx:
+        song_title = df_tracks.at[i, "Song_title"]
+        artist_name = df_tracks.at[i, "Artist"]
+        
+        lyrics = get_genius_lyrics(song_title, artist_name, ACCESS_TOKEN)
+        df_tracks.at[i, "lyrics"] = lyrics
+
+        time.sleep(response_wait_time)
+
+
+    # Get the dataframe with lyrics info
+    df_lyrics = df_tracks[df_tracks["lyrics"]!=" "]
+
+    # Save the updated version of main data to local folder
+    df_tracks.to_csv("../../data/spotify_tracks_with_lyrics.csv", index=False)
+
+    # Save the latest version of the data with acquired lyrics
+    datenow = datetime.now().strftime("%Y-%m-%d")
+    timenow = datetime.now().strftime("%H-%M-%S")
+    df_lyrics.to_csv(f"../../data/spotify_lyrics_{datenow}_{timenow}.csv", index=False)
+
+    print(len(df_lyrics))
