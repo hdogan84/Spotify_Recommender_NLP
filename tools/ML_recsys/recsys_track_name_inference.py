42ce6777d35a0c3ba1c2329686fe110716280f1d
hdogan84 <dgnhkn@gmail.com>
2024-09-28 16:32:45 +0200

    Rearrange recsys script. (make compatible for unit testing)

--- tools/ML_recsys/recsys_track_name_inference.py
+++ tools/ML_recsys/recsys_track_name_inference.py
@@ -1,72 +1,52 @@
 import numpy as np
 import pandas as pd
 import pickle
+import sys
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics.pairwise import cosine_similarity
 
-folder_name = "../../data/ML_models/"
-model_name = 'RecSys_track_name.pkl'
 
+def get_recommendation_title(track_name) :
 
-with open(folder_name+model_name, 'rb') as file:
-    model_dict = pickle.load(file)
+    folder_name = "../../data/ML_models/"
+    model_name = 'RecSys_track_name.pkl'
 
- 
-# Use one split from Spotify (other than fold 0) as test data
-df_spotify = pd.read_csv("../../data/spotify_track_ids.csv")
-df_spotify = df_spotify.drop("Unnamed: 0", axis=1)
-df_spotify = df_spotify.rename(columns={"Song_title": "track_name", "Artist": "artist_name"})
-df_spotify.track_name = df_spotify.track_name.astype(str)
-df_spotify.track_name = df_spotify.track_name.apply(lambda x: x.lower())
-df_spotify.artist_name = df_spotify.artist_name.astype(str)
-df_spotify.artist_name = df_spotify.artist_name.apply(lambda x: x.lower())
+    with open(folder_name+model_name, 'rb') as file:
+        model_dict = pickle.load(file)
 
+    # Load model variables from pickle
+    df = model_dict["dataframe"]
+    vectorizer = model_dict["vectorizer"]
+    count_matrix = model_dict["count_matrix"]
+    count_matrix.shape
 
-skf = StratifiedKFold(n_splits=5, random_state=421, shuffle=True)
-
-for i, (train_ind, test_ind) in enumerate(skf.split(df_spotify.track_name, df_spotify.artist_name)):
-    df_spotify.loc[test_ind,"fold"] = i
-
-df_spotify.fold = df_spotify.fold.astype(int)
-
- 
-# Choose a Spotify split as test data
-fold_number = 2
-
-df_test = df_spotify[df_spotify.fold == 2]
-df_test = df_test[["track_name", "artist_name"]].reset_index(drop=True)
-df_test = df_test.drop_duplicates()
-df_test = df_test.reset_index(drop=True)
-
-
- 
-# Load model variables from pickle
-df = model_dict["dataframe"]
-vectorizer = model_dict["vectorizer"]
-count_matrix = model_dict["count_matrix"]
-count_matrix.shape
-
- 
-
-def get_recommendation_title(ind) :
-    feature_vector = vectorizer.transform(pd.DataFrame(df_test["track_name"]).iloc[ind])
+    single_df = pd.DataFrame([track_name], columns=["track_name"])
+    feature_vector = vectorizer.transform(single_df.iloc[0])
     result = np.dot(feature_vector, count_matrix.T).toarray()[0]
     ind1 = np.argsort(result)[::-1][:10]
+    
     return df.loc[ind1]
-
 
 
 if __name__ == "__main__":
     
-    ind = 900
+    # Use one split from Spotify (other than fold 0) as test data
+    fold_number = int(sys.argv[1])
+
+    # use a random index to choose a track name
+    ind = int(sys.argv[2])
+
+    # load the test data 
+    df_test = pd.read_csv(f"../../data/spotify_tracks_500k_fold{fold_number}.csv")
+
     song=df_test.loc[ind]["track_name"]
     singer=df_test.loc[ind]["artist_name"]
     print("The input singer and song name is: ")
     print(singer, "  ", song)
 
-    recs = get_recommendation_title(ind)
+    recs = get_recommendation_title(song)
     print("\nRecommendations:")
     print(recs)
     	
